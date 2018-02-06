efaef28a419c5bc7ab87518c298bc0c0acdb1421
Pouyan1989 <33005146+Pouyan1989@users.noreply.github.com>
2018-02-06 10:20:39 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,39 +1,26 @@
 <!DOCTYPE html>
-<html<?php language_attributes();?>>        <!--specifies which language is being used -->
+<html <?php language_attributes();?>>
 
-<head>     
-    <meta charset="<?php bloginfo('charset');?>">          <!--displays the encoding for pages and feed in seeting reading-->
-    <meta name="viewport" content="width=device-width">     <!--makes our site responsive-->
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width">
     <title>
-        <?php bloginfo('name');?>    <!--this our website name not optimal for search engine-->
+        <?php bloginfo('name');?>
     </title>
-    <?php wp_head();?>  <!--if we later add any plugins to wordpress this will play a role -->
+    <?php wp_head();?>
 </head>
 
-<body <?php body_class();?>>    <!--lets us target different pages with css easily-->
+<body <?php body_class();?>>
     <div class="container">
-
         <!--site header-->
         <header class="site-header">
-
-      <Div class="hd-search">
-        <?php get_search_form(); ?>
-
-      </Div>
-
-
-
-            <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a></h1>  
-            <h5><?php bloginfo('description');?><?php if (is_page('Blog')){ ?>    <!--this is conditional logic in case if we wanna affect only the blog page-->
-
-                - thank you for watching our work 
-
-                <?php }?></h5>  
+            <h1><a href="<?php echo home_url();?>"><?php bloginfo('name');?></a></h1>
+            <h5><?php bloginfo('description');?></h5>
 			<nav class="site-nav">
 			  <?php
-			    $args=array('theme_location'=>'primary'); // loads the top header menu her that we have chosen as primary in admin panel
+			    $args=array('theme_location'=>'primary');
 				?>
-			 <?php wp_nav_menu($args);?> <!--outputs all the pages with this function ,but he have modified to put out desired pages with the argument-->
+			 <?php wp_nav_menu($args);?>
 			</nav>
 			
         </header>
--- index.php
+++ index.php
@@ -1,48 +1,15 @@
-<!--put the loop code-->
-<?php get_header();       /* loads the header here  */
- 
- if (have_posts()):   /*it says if we have posts .do this  */
-
- 	while(have_posts()):the_post();?>  
-
-    <article class="post">
-
-    	<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-    	<p class="post-info"><?php the_time('F jS,Y g:i a');?> | by <a href="<?php get_author_posts_url(get_the_author_meta('ID'));?>"><?php the_author();?></a> | Posted in 
-
-			<?php 
-			$categories=get_the_category();
-			$seperator=",";
-			$output='';
-
-			if ($categories) {
-
-				foreach ($categories as $category) {
-					
-					$output .='<a href="' .get_category_link($category->term_id). '">'.$category->cat_name.'</a>'.$seperator;
-				}
-
-
-					echo trim($output,$seperator); // will remove anything at the beggining or the end anything that resembles our seperator
-
-			}
-
-
-			?></p>
-	    
-
-
+<?php get_header(); //put the loop code
+ if (have_posts()): while(have_posts()):the_post(); ?>
+<article class="post">
+    <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 	    <?php the_content();?>
-	</article>
- 	
-
-	<?php endwhile;
-
-else:
-
-		echo'<p>no content found <p>';     /* and if we do not have posts , print this one   */
-
-endif;
-
- get_footer(); ?>
+	 </article>
+		
+ <?php endwhile;
+	
+	else :
+	    echo '<p>No content found</p>';
+    endif;
+ get_footer();
  
+?>
